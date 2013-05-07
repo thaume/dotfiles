87c07dd445cadfa10c0a2c1b11622f16d7c15970
thaume <tom.coquereau@gmail.com>
2013-05-07 18:20:07 +0200

    Update dotfiles

--- aliases.ps1
+++ aliases.ps1
@@ -6,3 +6,9 @@ Set-alias subl sublime
 
 # Mac OS open alias
 Set-alias open explorer
+
+# Launch MongoDB
+Set-alias mongo "C:\mongodb\bin\mongod.exe"
+
+# Set path for  shell script
+Set-alias gatling "C:\gatling\bin\gatling.sh"
--- path_utils.ps1
+++ path_utils.ps1
@@ -2,19 +2,19 @@
 #####################################
 
 function writePath {
-  $path = ""
+  # $path = ""
 
-  #Split the path to only display the parent folder
-  $pathbits = ([string]$pwd).split("\", [System.StringSplitOptions]::RemoveEmptyEntries)
-  if($pathbits.length -eq 1) {
-    $path = $pathbits[0] + "\"
-  } else {
-    $path = $pathbits[$pathbits.length - 1]
-  }
+  # #Split the path to only display the parent folder
+  # $pathbits = ([string]$pwd).split("\", [System.StringSplitOptions]::RemoveEmptyEntries)
+  # if($pathbits.length -eq 1) {
+  #   $path = $pathbits[0] + "\"
+  # } else {
+  #   $path = $pathbits[$pathbits.length - 1]
+  # }
 
   # Get user name / current directory
   $userLocation = $env:username + '@' + [System.Environment]::MachineName
   $host.UI.RawUi.WindowTitle = $userLocation
   Write-Host($userLocation) -nonewline -foregroundcolor Magenta 
-  Write-Host(" "+$path) -nonewline -foregroundcolor White
+  Write-Host(" "+[string]$pwd) -nonewline -foregroundcolor White
 }
